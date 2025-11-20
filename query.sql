9e754cbe2459949b0c42a94cfea7ef10a727d090
LucasB-07 <99080017@mydavinci.nl>
2025-11-20 12:35:21 +0100

    Delete query.sql

--- query.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Users (
-    ID INT PRIMARY KEY,
-    Name VARCHAR(50),
-    Age INT
-);
-
-INSERT INTO Users (ID, Name, Age) VALUES (1, 'Alice', 25);
-INSERT INTO Users (ID, Name, Age) VALUES (2, 'Bob', 30);
-SELECT * FROM Users;
